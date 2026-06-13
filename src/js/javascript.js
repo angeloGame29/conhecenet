578b12ec434abd27c228c8400a29254c76696d19
João Ângelo <angelo.silva@dcx.ufpb.br>
2026-06-13 19:26:00 -0300

    dark mode

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -66,3 +66,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        toggle.innerHTML = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggle.innerHTML = "🌙";
+    }
+});
+
+// Mantém o tema salvo
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggle.innerHTML = "☀️";
+}
