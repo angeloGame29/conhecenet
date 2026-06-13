30a429c761d32a5143208b923738f89570b0cbf5
angeloGame29 <angelo.silva@dcx.ufpb.br>
2026-06-13 02:28:32 -0300

    Add files via upload

---
+++ src/js/javascript.js
@@ -0,0 +1,68 @@
+const questions = document.querySelectorAll('.faq-question');
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+
+        const answer = question.nextElementSibling;
+        const isOpen = answer.style.maxHeight;
+
+        // Fecha todos
+        document.querySelectorAll('.faq-answer').forEach(item => {
+            item.style.maxHeight = null;
+        });
+
+        document.querySelectorAll('.faq-question').forEach(item => {
+            item.classList.remove('active');
+        });
+
+        // Abre o clicado
+        if (!isOpen) {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+            question.classList.add('active');
+        }
+    });
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-Links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-Links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        menuToggle.classList.remove('active');
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (
+        !navLinks.contains(e.target) &&
+        !menuToggle.contains(e.target)
+    ) {
+        navLinks.classList.remove('active');
+        menuToggle.classList.remove('active');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    ScrollReveal().reveal('#hero-stats li', {
+        origin: 'bottom',
+        duration: 1000,
+        distance: '20%',
+        interval: 700
+    });
+
+    ScrollReveal().reveal('#servicos-grids article', {
+        origin: 'left',
+        duration: 1000,
+        distance: '55px',
+        interval: 650
+    });
+
+});
